456b0953f9ab343536d55ca4bf5734268b235661
WeWang5000 <47341315+WeWang5000@users.noreply.github.com>
2019-04-03 16:14:29 -0700

    I work HTML, CSS and Javascript for my resume.

---
+++ main.js
@@ -0,0 +1,18 @@
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function topFunction(x) {
+  document.body.scrollTop = x;
+  document.documentElement.scrollTop = x;
+}
